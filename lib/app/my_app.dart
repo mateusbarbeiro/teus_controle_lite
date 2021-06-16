5b610e39e41cf76c6325251eeaacfddc7a817cee
mateus barbeiro <mateus_garcia01@hotmail.com>
2021-06-16 20:57:08 -0300

    first commit

---
+++ lib/app/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:teus_controle_lite/app/view/home.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
